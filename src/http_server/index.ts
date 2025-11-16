d6ba76e27d13092c2f42c4ee7abf63f327ad5889
Alexander Kuzmin <kuzminalex96@icloud.com>
2025-11-16 19:37:08 +0100

    final final comit. Resplver issues with path wich could make problems on windows pc

--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -1,18 +1,16 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as http from 'http';
-// import { fileURLToPath } from 'url';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname_corrected = path.dirname(__filename);
 
 export const httpServer = http.createServer(function (
   req: http.IncomingMessage,
   res: http.ServerResponse
 ) {
-  // const projectRoot = path.resolve(__dirname_corrected, '../../');
-  const projectRoot = path.resolve(__dirname, '../../'); // Используйте __dirname
-  const file_path = projectRoot + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
+  const requestedUrl = req.url === '/' ? '/index.html' : (req.url || '');
+  const safeRelativePath = path.normalize(path.join('/front', requestedUrl))
+                                    .replace(/^(\.\.[\/\\])+/, '');
+  const projectRoot = path.resolve(__dirname, '../../');
+  const file_path = path.join(projectRoot, safeRelativePath);
 
   fs.readFile(file_path, function (err, data) {
     if (err) {
